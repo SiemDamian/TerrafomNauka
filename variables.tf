695a7e925050092acbffafaa270df7888e846647
ds1 <damian.siemieniec@softnet.com.pl>
2024-04-26 11:52:48 +0200

    Dodanie variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,19 @@ variable "aws_secret_access_key" {
   description = "AWS Secret Access Key"
   type        = string
 }
+
+variable "server_port" { 
+ description = "Numer portu używany przez serwer dla żądań HTTP" 
+ type = number 
+ default = 8080 
+} 
+
+output "public_ip" { 
+ value = aws_instance.example.public_ip 
+ description = "Publiczny adres IP serwera WWW" 
+} 
+
+output "alb_dns_name" { 
+ value = aws_lb.example.dns_name 
+ description = "Nazwa domeny mechanizmu równoważenia obciążenia" 
+} 
